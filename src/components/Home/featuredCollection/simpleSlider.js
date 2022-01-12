c910c8222c3571489c4b2b6b0d2bbaefe3ce5f9e
hadis <hadissarlak001379@gmail.com>
2022-01-13 00:15:50 +0330

    responsive Home

--- src/components/Home/featuredCollection/simpleSlider.js
+++ src/components/Home/featuredCollection/simpleSlider.js
@@ -9,13 +9,41 @@ const SimpleSlider = ({ data }) => {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
+    speed: 500,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
+
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
+  
   return (
     <section>
       <Slider {...settings}>
